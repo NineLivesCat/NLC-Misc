810d9c3f42b278cced41557f9ffb6bc779ef5e0e
NineLivesCat <esdcasdcd@126.com>
2021-04-09 17:02:06 +0800

    Improve HUI

--- script/setup/gcc_arm.sh
+++ script/setup/gcc_arm.sh
@@ -1,4 +1,4 @@
-read -p "Download package from https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm/downloads"
+read -p "Download package from https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm/downloads "
 sudo tar -xjf gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2
 sudo mv gcc-arm-none-eabi-10-2020-q4-major gcc-arm-none-eabi
 sudo cp -r ./gcc-arm-none-eabi ~
--- script/setup/stm32cubemax.sh
+++ script/setup/stm32cubemax.sh
@@ -3,8 +3,8 @@ sudo apt install \
     openjdk-11-jdk
 
 echo "Please go to https://www.st.com/en/development-tools/stm32cubemx.html download..."
-echo "unzip and run SetupSTM32CubeMX-<Version>"
+echo "unzip and run SetupSTM32CubeMX-<Version> "
 
 ## Ozone and SystemView
 echo "Ozone and SystemView"
-echo "Dowanload and install from https://www.segger.com/downloads/jlink/"
+echo "Dowanload and install from https://www.segger.com/downloads/jlink/ "
